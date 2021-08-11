b763dbddc4b148530824090e2eee94f20c19a926
Natividadcv <cv0195042019@unab.edu.sv>
2021-08-10 22:12:27 -0600

    Agregando BD

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
     session_start();
   
     $conexion = mysqli_connect(
-        'localhost:3307',
+        'localhost:3307', //Si no te funciona borra los siguiente :3307
         'root',
         '',
         'todolist'
